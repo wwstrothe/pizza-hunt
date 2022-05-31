572b830719159824dc75998a830956739a52b879
wwstrothe <95983821+wwstrothe@users.noreply.github.com>
2022-05-31 12:03:01 -0700

    added updatePizza in pizza-controller

--- controllers/pizza-controller.js
+++ controllers/pizza-controller.js
@@ -28,6 +28,26 @@ const pizzaController = {
         res.status(400).json(err);
       });
   },
+
+  // create pizza
+  createPizza({ body }, res) {
+    Pizza.create(body)
+      .then(dbPizzaData => res.json(dbPizzaData))
+      .catch(err => res.status(400).json(err));
+  },
+
+  //update pizza by id
+  updatePizza({ params, body }, res) {
+    Pizza.findOneAndUpdate({ _id: params.id }, body, { new: true })
+      .then(dbPizzaData => {
+        if(!dbPizzaData) {
+          res.status(400).json({ message: 'No pizza found with this id!' });
+          return;
+        }
+        res.json(dbPizzaData);
+      })
+      .catch(err => res.status(400).json(err))
+    },
 }
 
 module.exports = pizzaController;

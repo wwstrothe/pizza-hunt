ece4b9eaaa6f3cf28255933dc20af88526b3150c
wwstrothe <95983821+wwstrothe@users.noreply.github.com>
2022-05-31 15:07:09 -0700

    added comment-routes in /routes/api as well as updated comment-controller to remove comment rather then add another id

--- controllers/comment-controller.js
+++ controllers/comment-controller.js
@@ -32,7 +32,7 @@ const commentController = {
       }
       return Pizza.findOneAndUpdate(
         { _id: params.pizzaId },
-        { $push: { comments: params.commentId } },
+        { $pull: { comments: params.commentId } },
         { new: true }
       );
     })

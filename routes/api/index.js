dd56ca570f76a557f3e76ffcc3c2cc041a348bc1
wwstrothe <95983821+wwstrothe@users.noreply.github.com>
2022-05-31 13:08:16 -0700

    connected to front end by updating add-pizza.js

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,6 +2,6 @@ const router = require('express').Router();
 const pizzaRoutes = require('./pizza-routes');
 
 // add prefix of `/pizzas` to routes created in `pizza-routes.js`
-router.use('/pizza', pizzaRoutes);
+router.use('/pizzas', pizzaRoutes);
 
 module.exports = router;

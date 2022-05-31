39ac6f86825cb3ab2d32fb75be45b5c8be70dd96
wwstrothe <95983821+wwstrothe@users.noreply.github.com>
2022-05-31 14:41:26 -0700

    added addComment and removeComment in comment-controller.js

---
+++ controllers/comment-controller.js
@@ -0,0 +1,50 @@
+const res = require('express/lib/response');
+const { Comment, Pizza } = require('../models')
+
+const commentController = {
+  // add comment to pizza
+  addComment({ params, body }, res) {
+    console.log(body);
+    Comment.create(body)
+      .then(({ _id }) => {
+        return Pizza.findOneAndUpdate(
+          { _id: params.pizzaId },
+          { $push: { comments: _id } },
+          { new: true }
+        );
+      })
+      .then(dbPizzaData => {
+        if(!dbPizzaData) {
+          res.status(404).json({ message: 'No pizza found with this id!' });
+          return
+        }
+        res.json(dbPizzaData)
+      })
+      .catch(err => res.json(err));
+  },
+  
+  // remove comment from pizza
+  removeComment({ params }, res) {
+    Comment.findOneAndDelete({ _id: params.commentId })
+    .then(deletedComment => {
+      if(!deletedComment) {
+        res.status(404).json({ message: 'No comment with this id!' });
+      }
+      return Pizza.findOneAndUpdate(
+        { _id: params.pizzaId },
+        { $push: { comments: params.commentId } },
+        { new: true }
+      );
+    })
+    .then(dbPizzaData => {
+      if(!dbPizzaData) {
+        res.status(404).json({ message: 'No pizza found with this id!' })
+        return;
+      }
+      res.json(dbPizzaData)
+    })
+    .catch(err => res.json(err))
+  }
+};
+
+module.exports = commentController;
